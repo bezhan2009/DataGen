31665a61388241c4ddef4f04f4955ab2451007ba
umarkasenov <casenov2101@icloud.com>
2024-05-28 23:37:37 +0600

    first-commit

---
+++ data_gen_errors/errors_for_utils_data.py
@@ -0,0 +1,28 @@
+class LenGetError(Exception):
+    """
+    Класс для пользовательской ошибки.
+    """
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"LenGetError: {self.message}"
+
+
+class LenNotProvidedError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"LenNotProvided: {self.message}"
+
+
+class TypeGetError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"TypeGetError: {self.message}"
