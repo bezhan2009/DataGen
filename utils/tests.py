7d9c6d924320c313aa74eadbd421f4ceb641a47e
bezhan2009 <karimovbezan0@gmail.com>
2024-05-28 18:42:21 +0300

    add phone numbers and refactor tests

--- utils/tests.py
+++ utils/tests.py
@@ -1,6 +1,7 @@
 import logging
 import unittest
 
+from .phone_number_generator import generate_phone_numbers
 from datetime import datetime
 from .date_generator import generate_date
 from .random_data import random_data
@@ -100,6 +101,38 @@ class TestRandomDates(unittest.TestCase):
         self.assertEqual(generated_date.hour, 0)
         self.assertEqual(generated_date.minute, 0)
         self.assertEqual(generated_date.second, 0)
+
+
+class TestGeneratePhoneNumbers(unittest.TestCase):
+    def test_single_number_default_length(self):
+        numbers = generate_phone_numbers(1)
+        logging.debug("Generated phone numbers: %s", numbers)
+        self.assertEqual(len(numbers), 1)
+        self.assertTrue(all(len(num) == 12 for num in numbers))  # 1 (country code) + 10 (number length) + 1 (+ sign)
+
+    def test_multiple_numbers(self):
+        numbers = generate_phone_numbers(44, 5)
+        logging.debug("Generated phone numbers: %s", numbers)
+        self.assertEqual(len(numbers), 5)
+        self.assertTrue(all(len(num) == 13 for num in numbers))  # 2 (country code) + 10 (number length) + 1 (+ sign)
+
+    def test_custom_length(self):
+        numbers = generate_phone_numbers(91, 3, 8)
+        logging.debug("Generated phone numbers: %s", numbers)
+        self.assertEqual(len(numbers), 3)
+        self.assertTrue(all(len(num) == 11 for num in numbers))  # 2 (country code) + 8 (number length) + 1 (+ sign)
+
+    def test_invalid_country_code(self):
+        with self.assertRaises(ValueError):
+            generate_phone_numbers(-1)
+
+    def test_invalid_number_count(self):
+        with self.assertRaises(ValueError):
+            generate_phone_numbers(1, 0)
+
+    def test_invalid_phone_length(self):
+        with self.assertRaises(ValueError):
+            generate_phone_numbers(1, 1, -5)
 
 
 if __name__ == '__main__':
