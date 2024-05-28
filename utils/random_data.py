dd5aeec47180adcc1041fa4a5bc6fdf4367582aa
Grade <aborecevskij94@gmail.com>
2024-05-28 13:33:32 +0300

    fix: bug with randint range. fixed data types

--- utils/random_data.py
+++ utils/random_data.py
@@ -1,21 +1,24 @@
 import random
+import typing
 
-import str_generator
 from data_gen_errors import errors_for_utils_data
+from utils import str_generator
 
 
-def random_data(data_type, length=0):
+def random_data(
+        data_type: typing.Union[int, float, str],
+        length: int = 0
+) -> typing.Union[int, float, str]:
     if data_type == int:
-        negative = random.randint(0, 1)
-        if negative:
-            return random.randint(-10, -100)
-        else:
-            return random.randint(10, 100)
-    if data_type == 'float':
-        random_int = random.randint(10, 100)
+        return random.randint(-100, 100)
+
+    elif data_type == float:
+        random_int = random.randint(-100, 100)
         return random_int + (random.randint(10, 99) / 100)
-    if data_type == 'str':
-        if len != 0:
-            str_generator.generate_string(length)
+
+    elif data_type == str:
+        if length != 0:
+            return str_generator.generate_string(length)
+
         else:
-            errors_for_utils_data.LenNotProvidedError("len data is not provided!!!")
+            raise errors_for_utils_data.LenNotProvidedError("Data length is not provided.")
