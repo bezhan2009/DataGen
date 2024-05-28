76241d9db936eaf7e4385283ecbbab59385f7fd2
Grade <aborecevskij94@gmail.com>
2024-05-28 13:05:41 +0300

    upd: changed generating string logic Using string module to get symbols list

--- utils/some_str.py
+++ utils/some_str.py
@@ -1,5 +1,6 @@
 import random
-import len_get
+import string
+
 
 # class Name:
 #     def __init__(self, name, surname, age):
@@ -8,27 +9,18 @@ import len_get
 #         self.age = age
 
 
-class Data:
-    def __init__(self, letters, nums):
-        self.letters = letters
-        self.nums = nums
+# class Data:
+#     def __init__(self, letters, nums):
+#         self.letters = letters
+#         self.nums = nums
 
 
-def get_some_str(len_word):
-    data = [
-        Data(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-              'v', 'w', 'x', 'y', 'z'], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]),
-        Data(['!', '@', '"', '#', '$', '%', '^', '&', '*', '(', ')', '?'],
-             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]),
-    ]
-
-    word = ""
-
-    while len(word) < len_word:
-        data_choice = random.choice(data)
-        letter_choice = random.choice(data_choice.letters)
-        number_choice = random.choice(data_choice.nums)
-        word += random.choice([letter_choice, str(number_choice)])
-
+def get_some_str(
+        length: int
+) -> str:
+    """
+    Get random string
+    """
+    symbols = string.ascii_letters + string.digits + string.punctuation
+    word = [random.choice(symbols) for _ in range(length)]
     return word
-
