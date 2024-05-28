094ceb051006ddb70005532e0c52f1b07f10583b
Grade <aborecevskij94@gmail.com>
2024-05-28 15:36:38 +0300

    add: tests for ip generator

--- utils/tests.py
+++ utils/tests.py
@@ -1,6 +1,7 @@
 import logging
 import unittest
 
+from .ip_generator import ipv4_generate, ipv6_generate, Ip
 from .random_data import random_data
 from .str_generator import generate_string
 
@@ -48,6 +49,32 @@ class TestRandomData(unittest.TestCase):
     def test_bool_error(self):
         with self.assertRaises(ValueError):
             random_data(bool)
+
+
+class TestIpGenerator(unittest.TestCase):
+    def test_ipv4_generate(self):
+        generated_ipv4: Ip = ipv4_generate()
+        logging.debug(f"Generated ipv4: {generated_ipv4}")
+        self.assertEqual(generated_ipv4.type, "ipv4")
+        self.assertTrue(generated_ipv4.valid())
+
+    def test_ipv4_generate_invalid(self):
+        generated_ipv4: Ip = ipv4_generate(False)
+        logging.debug(f"Generated ipv4: {generated_ipv4}")
+        self.assertEqual(generated_ipv4.type, "ipv4")
+        self.assertFalse(generated_ipv4.valid())
+
+    def test_ipv6_generate(self):
+        generated_ipv6: Ip = ipv6_generate()
+        logging.debug(f"Generated ipv6: {generated_ipv6}")
+        self.assertEqual(generated_ipv6.type, "ipv6")
+        self.assertTrue(generated_ipv6.valid())
+
+    def test_ipv6_generate_invalid(self):
+        generated_ipv6: Ip = ipv6_generate(False)
+        logging.debug(f"Generated ipv6: {generated_ipv6}")
+        self.assertEqual(generated_ipv6.type, "ipv6")
+        self.assertFalse(generated_ipv6.valid())
 
 
 if __name__ == '__main__':
