ea2099edcaf59074d734e3f574c5410e64ca32e6
Grade <aborecevskij94@gmail.com>
2024-05-28 13:00:07 +0300

    fix: error messages

--- utils/len_get.py
+++ utils/len_get.py
@@ -9,12 +9,12 @@ def get_data(len_data):
     """
 
     if len_data is None:
-        return errors_for_utils_data.LenNotProvidedError("len data is not provided!!!")
+        return errors_for_utils_data.LenNotProvidedError("Data length is not provided")
     if len_data < 0:
-        return errors_for_utils_data.LenGetError("len cannot be negative!!!")
+        return errors_for_utils_data.LenGetError("Length cannot be negative")
     if len_data == 0:
-        return errors_for_utils_data.LenGetError("len cannot be 0!!!")
+        return errors_for_utils_data.LenGetError("Length cannot be 0")
     if len_data >= 1:
         return
     if len_data >= 999:
-        return errors_for_utils_data.LenGetError("len cannot be greater than 999!!!")
+        return errors_for_utils_data.LenGetError("Length cannot be greater than 999")
