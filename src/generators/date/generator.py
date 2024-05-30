519ddfcd08826740b3bfdabd48a6e86c593f2d00
bezhan2009 <karimovbezan0@gmail.com>
2024-05-30 18:57:48 +0300

    add custom errors

--- src/generators/date/generator.py
+++ src/generators/date/generator.py
@@ -1,9 +1,11 @@
-import random
+import random as r
 from datetime import datetime
+
+from src.errors.generator_errors import errors
 
 
 def generate_date(day: int = 0, month: int = 0, year: int = 0, hour: int = None, minute: int = None,
-                  second: int = None) -> GeneratedDate:
+                  second: int = None):
     """
     Generates a random date and optional time.
 
@@ -13,14 +15,35 @@ def generate_date(day: int = 0, month: int = 0, year: int = 0, hour: int = None,
     :param hour: Hour of the day. If None, it is not included in the output.
     :param minute: Minute of the hour. If None, it is not included in the output.
     :param second: Second of the minute. If None, it is not included in the output.
-    :return: A GeneratedDate object with the specified or random values.
+    :return: A datetime object with the specified or random values.
     """
-    if day < 0 or day > 31:
-        raise ValueError("Invalid day. Please provide a day between 0 and 31.")
-    if month < 0 or month > 12:
-        raise ValueError("Invalid month. Please provide a month between 0 and 12.")
-    if hour is not None and (hour < 0 or hour > 23):
-        raise ValueError("Invalid hour. Please provide an hour between 0 and 23.")
-    if minute is not None and (minute < 0 or minute > 59):
-        raise ValueError("Invalid minute. Please provide a minute between 0 and 59.")
-    if second is not None and (
+    if day > 28 and month == 2:
+        raise errors.DateGetError("Invalid day for February. Please provide a day between 1 and 28.")
+    if day > 31:
+        raise errors.DateGetError("Invalid day. Please provide a day between 1 and 31.")
+    if month > 12 or month < 0:
+        raise errors.DateGetError("Invalid month. Please provide a month between 1 and 12.")
+    if hour is not None and (hour > 23 or hour < 0):
+        raise errors.DateGetError("Invalid hour. Please provide an hour between 0 and 23.")
+    if minute is not None and (minute > 59 or minute < 0):
+        raise errors.DateGetError("Invalid minute. Please provide a minute between 0 and 59.")
+    if second is not None and (second > 59 or second < 0):
+        raise errors.DateGetError("Invalid second. Please provide a second between 0 and 59.")
+
+    if day == 0:
+        day = r.randint(1, 28)
+    if month == 0:
+        month = r.randint(1, 12)
+    if year == 0:
+        year = r.randint(1970, 2050)
+
+    if hour is None and minute is None and second is None:
+        return datetime(year, month, day)
+    else:
+        if hour is None:
+            hour = 0
+        if minute is None:
+            minute = 0
+        if second is None:
+            second = 0
+        return datetime(year, month, day, hour, minute, second)
