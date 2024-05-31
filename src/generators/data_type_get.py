f09362237c99b7d248f08a3679fdba4030c6eacd
bezhan2009 <karimovbezan0@gmail.com>
2024-05-31 23:38:13 +0300

    add: docstrings to file data_type_get.py

--- src/generators/data_type_get.py
+++ src/generators/data_type_get.py
@@ -2,6 +2,23 @@ from src.errors.generator_errors import errors
 
 
 def type_get(type_element):
+    """
+    Returns the corresponding Python type for the given string representation.
+
+    Args:
+        type_element (str): The string representation of the data type.
+                            Accepted values are 'int', 'float', 'str', 'bool',
+                            'list', 'dict', 'tuple', and 'set'.
+
+    Returns:
+        type: The corresponding Python type if the type_element is recognized.
+              For example, if type_element is 'int', the function returns the int type.
+
+    Raises:
+        errors.TypeGetError: If the type_element is not recognized.
+                             The function raises a custom error indicating
+                             that the data type is unknown.
+    """
     if type_element == 'int':
         return int
     elif type_element == 'float':
@@ -18,8 +35,5 @@ def type_get(type_element):
         return tuple
     elif type_element == 'set':
         return set
-
     else:
         return errors.TypeGetError("Unknown data type")
-
-
