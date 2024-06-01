58b5f6260fe7493977fecf258fd4bff0190e485e
Grade <aborecevskij94@gmail.com>
2024-06-01 17:39:36 +0300

    refr: minor changes Removed import condition Added typehint to function parameter Removed unnecessary comments

--- src/generators/email/generator.py
+++ src/generators/email/generator.py
@@ -1,26 +1,18 @@
 import random
 
+from dataclass import GeneratedEmail
 from src.generators.email.utils import generate_string_with_digits
-try:
-    from .dataclass import GeneratedEmail
-except ImportError:
-    from dataclass import GeneratedEmail
 
 
-def generate_email(real_domain=True) -> GeneratedEmail:
+def generate_email(real_domain: bool = True) -> GeneratedEmail:
     """
     Generates a random email address.
-
-    Args:
-        real_domain (bool): If True, use a real domain from a predefined list.
-                            If False, generate a random domain.
-
-    Returns:
-        GeneratedEmail: An instance of GeneratedEmail containing the generated email.
+    :param real_domain: A boolean flag indicating whether to use real domain names or not.
+    :return: A randomly generated email address.
     """
     real_domains = ["gmail.com", "outlook.com", "yahoo.com"]
 
-    local_part = generate_string_with_digits(8, 4)  # 8 letters followed by 4 digits
+    local_part = generate_string_with_digits(8, 4)
     if real_domain:
         domain = random.choice(real_domains)
     else:
